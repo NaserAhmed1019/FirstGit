573567147b7c3b82c04714288cca53e90680bad7
Naser Ahmed <naserahmed1019@gmail.com>
2023-01-16 20:46:19 -0500

    Commit after changing Utils page

--- src/test/java/SelectM_Assignment/SelectMenue.java
+++ src/test/java/SelectM_Assignment/SelectMenue.java
@@ -8,13 +8,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SelectMenue {
     WebDriver driver;
 
-    SelectMenuePage  homePage;
+    SelectMenuePage homePage;
 
 
     @BeforeClass
@@ -29,17 +30,40 @@ public class SelectMenue {
     }
 
     @Test
-
     void SelectTest() throws InterruptedException {
         Thread.sleep(1000);
-
+// Select Value.
         driver.findElement(homePage.selectValueLocator).click();
         driver.findElement(homePage.selctingGroupB_Opt1).click();
         String selectValuetext = driver.findElement(homePage.selectValueAssert).getText();
         Assert.assertEquals(selectValuetext, "Group 2, option 1");
 
+// Select One.
+        driver.findElement(homePage.selectOneLocator).click();
+        driver.findElement(homePage.selectOneMs_Locator).click();
+        String selectOneText = driver.findElement(homePage.selectOneAssert).getText();
+        Assert.assertEquals(selectOneText, "Ms.");
+
+// Old Select Menu.
+        driver.findElement(homePage.oldSelectMenuLocator).click();
+        driver.findElement(homePage.oldMenuBlueLocator).click();
+        driver.findElement(homePage.oldSelectMenuLocator).click();
+        //   String blue = driver.findElement(homePage.oldSelectMenuLocator).getText();
+        //   System.out.println(blue);
+// Multi Select Locator.
+        driver.findElement(homePage.multiSelectLocator).click();
+        driver.findElement(homePage.multiSelectBlackLocator).click();
+        String black = driver.findElement(homePage.multiselectAssert).getText();
+        driver.findElement(homePage.multiSelectExitLo).click();// needs some change
+        Assert.assertEquals(black, "Black");
 
 
-
+// Standard Multi Select.
+        // driver.findElement(homePage.standardMultiSelectLocator).click();
+        driver.findElement(homePage.standardMultiSelectSaab).click();
     }
+@AfterClass
+    void wrapUp() {
+     //   driver.quit();
+}
 }
